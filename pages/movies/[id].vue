c8a0cce8c8f282f72a57a76e7e183bac870b20cb
Sungsoon Park <sungsoon.park@gmail.com>
2023-02-21 15:13:25 -0800

    useAsyncData optios

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,9 +1,20 @@
 <script setup>
+import { dataToEsm } from '@rollup/pluginutils';
+
 
 const route = useRoute()
 
 const { data } = await useAsyncData(() => {
   return $fetch(`http://www.omdbapi.com/?apikey=f2b4b417&i=${route.params.id}`)
+}, {
+  pick: ['Plot', 'Title'],
+  // transform(data) {
+  //   return {
+  //     Plot: data.Plot,
+  //     Title: data.Title
+  //   }
+  //   return data.Title
+  // }
 })
 
 </script>
