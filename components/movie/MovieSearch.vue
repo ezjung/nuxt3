c5dba059cb1cb8b0998535017c25cdd29ace5264
Sungsoon Park <sungsoon.park@gmail.com>
2023-02-21 15:03:36 -0800

    Getting from cache practice resolved.

--- components/movie/MovieSearch.vue
+++ components/movie/MovieSearch.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-  const query = ref("")
+  const query = ref("batman")
   const movies = ref([])
 
   async function search() {
@@ -8,19 +8,21 @@
     movies.value = Search
   }
 
+  search()
+
 </script>
 
 <template>
-  <div>
+  <div class="mx-24">
 
     <form @submit.prevent="search" class="flex justify-center">
       <input type="text" v-model="query" class="border mr-4">
       <button>Search</button>
     </form>
-    <ul class="flex wrap gap-10">
+    <ul class="grid grid-cols-4 wrap gap-x-4 gap-y-4 mt-16">
       <li v-for="movie in movies" :key="movie.imdbID">
         <NuxtLink :to="{ name: 'movies-id', params: { id: movie.imdbID } }">
-          <img :src="movie.Poster" :alt="movie.title" class="h-40 w-40 overflow-hidden">
+          <img :src="movie.Poster" :alt="movie.title" class="h-200 w-200 overflow-hidden">
         </NuxtLink>
       </li>
     </ul>    
